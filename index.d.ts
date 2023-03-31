7415e08de82d669c6cb3a8cc279555db05465e86
yarrajhansirani <128395001+yarrajhansirani@users.noreply.github.com>
2023-03-31 20:17:59 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,5 @@
 import type { PluginCreator } from 'postcss';
-declare type pluginOptions = {
-    preserve?: boolean;
-    enableProgressiveCustomProperties?: boolean;
-};
-declare const postcssPlugin: PluginCreator<pluginOptions>;
-export default postcssPlugin;
+declare const creator: PluginCreator<{
+    preserve: boolean;
+}>;
+export default creator;
