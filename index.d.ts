1769a9af527a1c5ba730f0ddd7422bdb02862bad
yarrajhansirani <128395001+yarrajhansirani@users.noreply.github.com>
2023-03-31 20:15:08 +0530

    Add files via upload

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,4 @@
-export { emitForest, emitForestLines, parseFunctionRanges, parseOffsets } from "./ascii";
-export { cloneFunctionCov, cloneProcessCov, cloneScriptCov, cloneRangeCov } from "./clone";
-export { compareScriptCovs, compareFunctionCovs, compareRangeCovs } from "./compare";
-export { mergeFunctionCovs, mergeProcessCovs, mergeScriptCovs } from "./merge";
-export { RangeTree } from "./range-tree";
-export { ProcessCov, ScriptCov, FunctionCov, RangeCov } from "./types";
+import type { PluginCreator } from 'postcss';
+import { pluginOptions } from './options';
+declare const creator: PluginCreator<pluginOptions>;
+export default creator;
